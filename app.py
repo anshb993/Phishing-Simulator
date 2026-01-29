c6248c7843a1077604c494dfc6749995fd199d91
anshb993 <anshbalve23@gmail.com>
2026-01-29 18:32:31 +0530

    added fake login page with trash ui

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
 from flask import Flask, render_template, request, redirect
 from datetime import datetime
+from email_templates import EMAILS #imports the file
 
 app = Flask(__name__)
-
+LOGS = []
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', emails=EMAILS)
 
+@app.route('/email/<int:email_id>')
+def viewEmail(email_id):
+    email = next((e for e in EMAILS if e["id"] == email_id), None)
+    return render_template('email.html', email=email)
 
-@app.route('/1')
-def viewEmail1():
-    return render_template('1.html')
+@app.route('/notice')
+def message():
+    return render_template('notice.html')
 
-@app.route('/2')
-def viewEmail2():
-    return render_template('2.html')
+@app.route("/login/<int:email_id>")
+def login(email_id):
+    return render_template("login.html", email_id=email_id)
 
-@app.route('/3')
-def viewEmail3():
-    return render_template('3.html')
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    email_id = request.form.get("email")
+    password = request.form.get("password")
+    LOGS.append({
+        "email_id": email_id,
+        "password": password,
+        "event": "form_submitted",
+        "time": datetime.now()
+    })
+    print(LOGS)
+    return redirect('/notice')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ email_templates.py
@@ -0,0 +1,30 @@
+EMAILS = [
+    {
+        "id": 1,
+        "sender": "SOC Team",
+        "email": "markbosco@gmail.com",
+        "subject": "Unusual login detected",
+        "body": "We detected a login attempt from a new device. Please verify immediately.",
+        "time": "08:15 AM",
+        "technique": "Urgency"
+    },
+    {
+        "id": 2,
+        "sender": "Jacob, Manager",
+        "email": "fantastic@gmail.com",
+        "subject": "Account verification required",
+        "body": "Your account will be restricted unless you verify your details.",
+        "time": "10:56 AM",
+        "technique": "Authority"
+    },
+    {
+        "id": 3,
+        "sender": "IT Support",
+        "email": "johnmarijones@hotmail.com",
+        "subject": "Password expires today",
+        "body": "Your password will expire today. Update it to avoid lockout.",
+        "time": "11:59 AM",
+        "technique": "Urgency"
+    },
+            
+]
