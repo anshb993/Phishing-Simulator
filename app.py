b53dd953ac50705a4815c83c59a653816414c5ad
anshb993 <anshbalve23@gmail.com>
2026-01-30 13:14:50 +0530

    added detailed notice page

--- app.py
+++ app.py
@@ -18,31 +18,39 @@ def main():
 @app.route('/email/<int:email_id>')
 def viewEmail(email_id):
     init_session()
+
     email = next((e for e in EMAILS if e["id"] == email_id), None)
     session["behaviour"].append("Email")
     risk = calculations(session["behaviour"], LOGS)
-    print("current risk: ",risk)
+    print("current risk: ", risk)
+
     return render_template('email.html', email=email)
 
 @app.route('/notice')
 def message():
     init_session()
+
+    entered_email = session.get("entered_email")
     session["behaviour"].append("Phished")
     risk = calculations(session["behaviour"], LOGS)
     print("current risk: ", risk)
-    return render_template('notice.html', risk=risk)
+
+    return render_template('notice.html', entered_email=entered_email)
 
 @app.route("/login/<int:email_id>")
 def login(email_id):
     init_session()
+
     session["behaviour"].append("Clicked")
     risk = calculations(session["behaviour"], LOGS)
     print("current risk: ", risk)
+
     return render_template("login.html", email_id=email_id)
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
     email_id = request.form.get("email")
+    session["entered_email"] = email_id
     password = request.form.get("password")
     LOGS.append({
         "email_id": email_id,
@@ -51,6 +59,7 @@ def submit():
         "time": datetime.now()
     })
     print(LOGS)
+
     return redirect('/notice')
 
 if __name__ == "__main__":
